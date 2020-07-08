9c90e3349d7d316cc7ab4626258233122cfb2b33
supertata <laetitiachira@gmail.com>
2020-07-08 10:25:23 +0100

    imported icons

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', ()=> {
  let long; 
  let lat;
+ let temperatureDescription = document.querySelector('.temperature-description');
+ let temperatureDegree = document.querySelector('.temperature-degree');
+ let locationTimezone = document.querySelector('.location-timezone');
 
  if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -16,7 +19,22 @@ window.addEventListener('load', ()=> {
     })
     .then(data => {
        console.log(data);
+       const {temperature, summary, icon} = data.currently;
+       //set DOM Elements from the API
+       temperatureDegree.textContent = temperature;
+       temperatureDescription.textContent = summary;
+       locationTimezone.textContent = data.timezone;
+         //Set Icons
+         setIcons(icon, document.querySelector(".icon"));
     });
 });
 }
+function setIcons(icon, iconID) {
+   const skycons = new Skycons ({color: "white"});
+   const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+   skycons.play();
+   return skycons.set(iconID, Skycons[currentIcon]);
+
+}
+
 });
