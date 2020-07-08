17fe383371b60b19167cad8f610eb072a8fbcc38
supertata <laetitiachira@gmail.com>
2020-07-08 10:36:43 +0100

    added change to C / F when click on Degree Section

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.addEventListener('load', ()=> {
  let temperatureDescription = document.querySelector('.temperature-description');
  let temperatureDegree = document.querySelector('.temperature-degree');
  let locationTimezone = document.querySelector('.location-timezone');
+ let temperatureSection = document.querySelector(".temperature");
+ const temperatureSpan = document.querySelector(".temperature span");
 
  if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -26,6 +28,14 @@ window.addEventListener('load', ()=> {
        locationTimezone.textContent = data.timezone;
          //Set Icons
          setIcons(icon, document.querySelector(".icon"));
+         //Change temperature to Celsius/Farenheit
+         temperatureSection.addEventListener("click", () => {
+             if (temperatureSpan.textContent === "F") {
+                 temperatureSpan.textContent = "C";
+             } else {
+                 temperatureSpan.textContent = "F";
+             }
+         })
     });
 });
 }
