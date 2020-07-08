d2c615a4fa3f75ef81c0b59c67dfac3b59ca3708
supertata <laetitiachira@gmail.com>
2020-07-08 11:02:50 +0100

    cleaned code

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-window.addEventListener('load', ()=> {
- let long; 
+let long; 
  let lat;
  let temperatureDescription = document.querySelector('.temperature-description');
  let temperatureDegree = document.querySelector('.temperature-degree');
  let locationTimezone = document.querySelector('.location-timezone');
  let temperatureSection = document.querySelector(".temperature");
  const temperatureSpan = document.querySelector(".temperature span");
+
+window.addEventListener('load', ()=> {
 
  if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -14,7 +15,6 @@ window.addEventListener('load', ()=> {
         const proxy = "https://cors-anywhere.herokuapp.com/";
         const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
     
-
  fetch(proxy + api)
     .then(response => {
         return response.json();
@@ -43,12 +43,23 @@ window.addEventListener('load', ()=> {
     });
 });
 }
-function setIcons(icon, iconID) {
-   const skycons = new Skycons ({color: "white"});
-   const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-   skycons.play();
-   return skycons.set(iconID, Skycons[currentIcon]);
-
-}
 
 });
+
+temperatureSection.addEventListener("click", () => {
+    if (temperatureSpan.textContent === "F") {
+        temperatureSpan.textContent = "C";
+        temperatureDegree.textContent = Math.floor(celsius);
+    } else {
+        temperatureSpan.textContent = "F";
+        temperatureDegree.textContent = temperature;
+    }
+})
+
+function setIcons(icon, iconID) {
+    const skycons = new Skycons ({color: "white"});
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+ 
+ }
